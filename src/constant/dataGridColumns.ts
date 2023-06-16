f4d77ffd5a4060770d4b12808845aa55e9686133
Le Doan Tan Tri <ledoantantri@gmail.com>
2023-06-16 21:21:06 +0700

    feat: add table of tour to detailRoute

---
+++ src/constant/dataGridColumns.ts
@@ -0,0 +1,75 @@
+import moment from "moment";
+
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+
+export const TOURIST_ROUTE_COLUMN = [
+	{
+		field: "_id",
+		headerName: "ID",
+		width: 250,
+	},
+	{
+		field: "name",
+		headerName: "Tourist route name",
+		width: 300,
+	},
+	{
+		field: "type",
+		headerName: "Type",
+		width: 120,
+	},
+	{
+		field: "reservationFee",
+		headerName: "Reservation Fee",
+		width: 150,
+	},
+	{
+		field: "route",
+		headerName: "Route",
+		valueGetter: (param) => param.row.route.join(" - "),
+		width: 250,
+	},
+	{
+		field: "createdAt",
+		headerName: "Created at",
+		width: 280,
+		valueGetter: (param) =>
+			moment(param.row.createdAt).format("DD MMMM YYYY, h:mm:ss a"),
+	},
+];
+
+export const TOUR_COLUMN: GridColDef[] = [
+	{
+		field: "_id",
+		headerName: "ID",
+		width: 250,
+	},
+	{
+		field: "name",
+		headerName: "Tour name",
+		width: 300,
+	},
+	{
+		field: "type",
+		headerName: "Type",
+		width: 120,
+	},
+	{
+		field: "from",
+		headerName: "Departure date",
+		width: 250,
+	},
+	{
+		field: "customer",
+		headerName: "Tickets",
+		width: 150,
+		description: "Total ticket",
+	},
+	{
+		field: "updatedAt",
+		headerName: "Updated at",
+		width: 280,
+		valueGetter: (param) =>
+			moment(param.row.updatedAt).format("DD MMMM YYYY, h:mm:ss a"),
+	},
+];
