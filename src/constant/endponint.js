754c450174304f7877638e543881146a17f56536
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-06-18 18:51:49 +0700

    feat: add PageTitle

--- src/constant/endponint.js
+++ src/constant/endponint.js
@@ -1,12 +1,11 @@
 const ENDPOINT = {
-    ON_BOARDING: '/onboarding',
-    HOME: '/',
-    AUTHENTICATION: '/authentication',
-    REGISTER: '/register',
-    TOURIST_ROUTE: '/tourist-route',
-    TOUR: '/tour',
-    VOUCHER: '/voucher',
-    VALIDATE_COMPANY: '/validate-company',
+	ON_BOARDING: "/onboarding",
+	HOME: "/",
+	AUTHENTICATION: "/authentication",
+	REGISTER: "/register",
+	TOURIST_ROUTE: "/tourist-route",
+	TOUR: "/tour",
+	VOUCHER: "/voucher",
 };
 
 export default ENDPOINT;
